0ac8a37eac15b7fe721c3563d6ec7d078878283b
sgechocosk <shun.pc@outlook.jp>
2025-06-17 16:55:36 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const rooms = {};
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
